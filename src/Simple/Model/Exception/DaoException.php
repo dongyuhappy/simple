31278cc2b8ece6bbad0b5bad942cdc1995a64095
dongyu <dongyuhappy@gmail.com>
2014-06-28 15:13:18 +0800

    Dao异常类的定义

---
+++ src/Simple/Model/Exception/DaoException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @link http://www.simple-inc.cn/
+ * @copyright Copyright (c) 2014 Simple-inc Software  inc
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+namespace Simple\Model\Exception;
+
+
+use Simple\Exception\SimpleException;
+
+class DaoException extends SimpleException
+{
+
+} 
